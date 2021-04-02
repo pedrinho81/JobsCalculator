fd258c590699fc83ee36b2a32b8241fc5a3617f8
unknown <1pedrohenri@gmail.com>
2021-04-02 18:23:36 -0300

    refatorando arquivos de rotas, criacao de controller e models

--- src/server.js
+++ src/server.js
@@ -7,8 +7,12 @@ server.set('view engine', 'ejs') //usando template engine
 //habilitar arquivos estáticobs
 server.use(express.static("public"))
 
+server.use(express.urlencoded({ extended: true }))
+
 //routes
 server.use(routes)
+
+//usar o req.body
 
 server.listen(3000, () => console.log("inicio servidor"))
 
